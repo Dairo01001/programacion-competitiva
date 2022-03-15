1214aa541b19ba3fef2e4fc8c816b365eecdee41
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-14 22:25:50 -0500

    Template C++

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> // Import general
+
+// Elimina algunas advertencias durante la compilacion (solo para VC++)
+#define _CRT_SECURE_NO_DEPRECATE
+
+using namespace std;
+
+// Atajos para tipos de datos "comunes" en concursos
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+#define INF 1000000000;
+
+// Opciones de memset habituales
+// memset(memo, -1, sizeof memo);              // Inicializando tabla memoizacion de DP con -1
+// memset(arr, 0, sizeof arr);                 // Vaciado de un array de enteros
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    return 0;
+}
